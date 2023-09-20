80590b558e08f3fe90bb1a4cf540e92a3f29f709
anne <leeyuhit@zhejianglab.com>
2023-09-20 16:32:04 +0800

    cros支持跨域

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,9 @@
 import Koa from "koa";
+import cors from "koa2-cors";
 import allRouters from "#router/index.js";
 
 const app = new Koa();
+app.use(cors())
 allRouters.forEach(route => {
   app.use(route.routes())
 })
